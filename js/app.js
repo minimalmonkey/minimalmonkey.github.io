a4eaace9230c702ef63294c53aea6fb3194cb6a1
Stephen Burgess <steve@minimalmonkey.com>
2014-08-13 17:42:10 -0700

    Added panel expand / shrink on hover.

--- js/app.js
+++ js/app.js
@@ -93,6 +93,7 @@ function Panels (options) {
 
 	this.el = document.getElementById(options.id);
 	this.panels = document.querySelectorAll('#' + options.id + ' .panel');
+	this.currentIndex = -1;
 
 	this.onMouseOver = this.onMouseOver.bind(this);
 	this.onMouseOut = this.onMouseOut.bind(this);
@@ -102,8 +103,60 @@ function Panels (options) {
 
 var proto = Panels.prototype;
 
+proto.addListenerToPanels = function () {
+	function callback (index) {
+		return function () {
+			this.onPanelMouseOver(index);
+		};
+	}
+	var len = this.panels.length;
+	while (len--) {
+		this.panels[len].addEventListener('mouseover', callback(len).bind(this), false);
+	}
+};
+
+proto.removeListenerFromPanels = function () {
+	var len = this.panels.length;
+	while (len--) {
+		//
+	}
+};
+
+proto.addExpandClass = function () {
+	this.panels[this.currentIndex].classList.add('is-expanded');
+
+	if (this.currentIndex > 0) {
+		this.panels[this.currentIndex - 1].classList.add('is-shrunk-left');
+	}
+
+	if (this.currentIndex < this.panels.length - 1) {
+		this.panels[this.currentIndex + 1].classList.add('is-shrunk-right');
+	}
+};
+
+proto.removeExpandClass = function () {
+	if (this.currentIndex > -1) {
+		this.panels[this.currentIndex].classList.remove('is-expanded');
+
+		if (this.currentIndex > 0) {
+			this.panels[this.currentIndex - 1].classList.remove('is-shrunk-left');
+		}
+
+		if (this.currentIndex < this.panels.length - 1) {
+			this.panels[this.currentIndex + 1].classList.remove('is-shrunk-right');
+		}
+	}
+};
+
+proto.onPanelMouseOver = function (index) {
+	if (this.currentIndex != index) {
+		this.removeExpandClass();
+		this.currentIndex = index;
+		this.addExpandClass();
+	}
+};
+
 proto.onMouseOver = function (evt) {
-	console.log('onMouseOver');
 	this.el.removeEventListener('mouseover', this.onMouseOver);
 	this.el.addEventListener('mouseout', this.onMouseOut, false);
 	this.el.classList.add('is-hovered');
@@ -111,20 +164,26 @@ proto.onMouseOver = function (evt) {
 
 proto.onMouseOut = function (evt) {
 	if (isMouseOut(evt)) {
-		console.log('onMouseOut');
 		this.el.removeEventListener('mouseout', this.onMouseOut);
 		this.el.addEventListener('mouseover', this.onMouseOver, false);
 		this.el.classList.remove('is-hovered');
+
+		if (this.currentIndex > -1) {
+			this.removeExpandClass();
+			this.currentIndex = -1;
+		}
 	}
 };
 
 proto.enable = function () {
 	this.el.addEventListener('mouseover', this.onMouseOver, false);
+	this.addListenerToPanels();
 };
 
 proto.disable = function () {
 	this.el.removeEventListener('mouseover', this.onMouseOver);
 	this.el.removeEventListener('mouseout', this.onMouseOut);
+	this.removeListenerFromPanels();
 };
 
 module.exports = Panels;
