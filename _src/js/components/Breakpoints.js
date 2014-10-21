51475717c7c55e471b6d98c9011ff266fc12390f
Stephen Burgess <steve@minimalmonkey.com>
2014-10-20 17:01:05 -0700

    Tidied up the new breakpoint stuff.

--- _src/js/components/Breakpoints.js
+++ _src/js/components/Breakpoints.js
@@ -5,87 +5,77 @@ var throttleEvent = require('../utils/throttleEvent');
 var EventEmitter = require('../components/EventEmitter');
 
 function Breakpoints() {
-	this.points = {};
-	this.generateFromJSON();
+	this._points = {};
+	this._currentPoints = {};
+	this._generateFromJSON();
 }
 
 var proto = Breakpoints.prototype = new EventEmitter();
 
-proto.generateFromJSON = function () {
+proto._generateFromJSON = function () {
 	var str = window.getComputedStyle(document.querySelector('html'), '::after').getPropertyValue('content');
-	str = str.substr(1, str.length-2);
-	var json = JSON.parse(str);
+	if (str) {
+		str = str.substr(1, str.length-2);
+		var json = JSON.parse(str);
+		this.addFromObject(json);
+	}
+};
 
-	// TODO: add an `add` mathod that takes an object and do the loop there rather than here
-	for (var name in json) {
-		this.add(name, json[name].from, json[name].to);
+proto.addFromObject = function (obj) {
+	for (var name in obj) {
+		this.add(name, obj[name].from, obj[name].to);
 	}
 };
 
 proto.add = function (name, from, to) {
-	this[name.toUpperCase()] = name; // TODO: check that doesn't already exist
-	this.points[name] = {
+	this[name.toUpperCase()] = name;
+	this._points[name] = {
 		from: from,
 		to: to
 	};
 };
 
 proto.remove = function (name) {
-	if (this.points[name]) {
+	if (this._points[name]) {
 		this[name.toUpperCase()] = undefined;
-		this.points[name] = undefined;
+		delete this._points[name];
+		delete this._currentPoints[name];
 	}
 };
 
 proto.contains = function (name) {
-	// TODO: don't like use of indexOf here - use an object instead
-	return this.currentPoints && this.currentPoints.indexOf(name) > -1;
+	return this._currentPoints[name];
 };
 
-proto.onResized = function () {
+proto._onResized = function () {
+	var isActive;
+	var changed = false;
 	var winWidth = window.innerWidth;
-	var current = [];
 
-	for (var point in this.points) {
-		if (this.points[point].from <= winWidth && this.points[point].to >= winWidth) {
-			current.push(point);
+	for (var point in this._points) {
+		isActive = this._points[point].from <= winWidth && this._points[point].to >= winWidth;
+		if (this._currentPoints[point] !== isActive) {
+			this._currentPoints[point] = isActive;
+			this.trigger((isActive ? 'in' : 'out') + ':' + point);
+			changed = true;
 		}
 	}
 
-	// TODO: use objects and JSON.stringify
-
-	if (current.join() !== this.currentPoints.join()) {
-
-		var i = this.currentPoints.length;
-		while (i--) {
-			if (current.indexOf(this.currentPoints[i]) < 0) {
-				this.trigger('out:' + this.currentPoints[i]);
-			}
-		}
-
-		i = current.length;
-		while (i--) {
-			if (this.currentPoints.indexOf(current[i]) < 0) {
-				this.trigger('in:' + current[i]);
-			}
-		}
-
-		this.currentPoints = current;
+	if (changed) {
 		this.trigger('update', {
-			points: this.currentPoints
+			points: this._currentPoints
 		});
 	}
 };
 
 proto.enable = function () {
-	this.currentPoints = [];
-	this.throttledResize = throttleEvent(this.onResized.bind(this), 50);
-	window.addEventListener('resize', this.throttledResize, false);
-	this.onResized();
+	this._throttledResize = throttleEvent(this._onResized.bind(this), 50);
+	window.addEventListener('resize', this._throttledResize, false);
+	this._onResized();
 };
 
 proto.disable = function () {
-	window.removeEventListener('resize', this.throttledResize);
+	window.removeEventListener('resize', this._throttledResize);
 };
 
 module.exports = new Breakpoints();
